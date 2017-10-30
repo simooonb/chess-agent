1ec5bca777fdc3e434daea2a93cf6cd6c1cc9b20
Simon <simon.bar@isen.yncrea.fr>
2017-10-30 13:33:26 -0400

    Initial commit

---
+++ ChessAgent/ChessAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChessAgent
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ ChessAgent/ChessAgent/WeightedEdge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace VacuumAgent
+{
+    public class WeightedEdge<T> : IComparable<WeightedEdge<T>>
+    {
+        public T Source { get; set; }
+        public T Destination { get; set; }
+        public long Weight { get; set; }
+
+        public WeightedEdge(T src, T dest, long weight)
+        {
+            Source = src;
+            Destination = dest;
+            Weight = weight;
+        }
+        
+        /// <summary>
+        /// Allow comparaison between edges.
+        /// </summary>
+        /// <param name="other">The other WeightedEdge instance.</param>
+        /// <returns>0 if both instances are equals, -1 if other > this, 1 otherwise.</returns>
+        public int CompareTo(WeightedEdge<T> other)
+        {
+            if (other == null)
+                return -1;
+
+            // Check if nodes are equal
+            if (Source.Equals(other.Source) && Destination.Equals(other.Destination))
+                return Weight.CompareTo(other.Weight);
+            
+            return -1;
+        }
+    }
+}
